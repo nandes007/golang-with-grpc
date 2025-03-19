632f32e5e4dd7771b2ea27abfa7e8ea17fe22139
Nandes Simanjuntak <fernandes.ariadi@jne.co.id>
2025-03-19 18:57:36 +0700

    initialize store struct

---
+++ simplebank/db/sqlc/store.go
@@ -0,0 +1,11 @@
+package db
+
+import (
+	"github.com/jackc/pgx/v5/pgxpool"
+)
+
+// Store provides all functions to execute db queries and transactions
+type Store struct {
+	*Queries
+	connPool *pgxpool.Pool
+}
