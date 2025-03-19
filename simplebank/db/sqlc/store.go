23a9a1327ddd73d4fbb10d6e08687c985feaa066
Nandes Simanjuntak <fernandes.ariadi@jne.co.id>
2025-03-19 18:58:11 +0700

    comment exec function

--- simplebank/db/sqlc/store.go
+++ simplebank/db/sqlc/store.go
@@ -6,6 +6,19 @@ import (
 
 // Store provides all functions to execute db queries and transactions
 type Store struct {
+	dbPool *pgxpool.Pool
 	*Queries
-	connPool *pgxpool.Pool
 }
+
+// NewStore creates a new Store
+func NewStore(dbPool *pgxpool.Pool) *Store {
+	return &Store{
+		dbPool:  dbPool,
+		Queries: New(dbPool),
+	}
+}
+
+// execTx executes a function within a database transaction
+// func (store *Store) execTx(ctx context.Context, fn func(*Queries) error) error {
+// 	tx, err := store.dbPool.BeginTx(context.Background(), nil)
+// }
