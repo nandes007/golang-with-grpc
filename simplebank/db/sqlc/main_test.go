87015ca89dc148082ce7294a4ad0d4d5318b415f
Nandes Simanjuntak <fernandes.ariadi@jne.co.id>
2025-03-24 16:29:07 +0700

    add viper library to load environment variable using config file

--- simplebank/db/sqlc/main_test.go
+++ simplebank/db/sqlc/main_test.go
@@ -7,17 +7,19 @@ import (
 	"testing"
 
 	"github.com/jackc/pgx/v5/pgxpool"
-)
-
-const (
-	dbURL = "postgresql://root:secret@localhost:5433/simple_bank?sslmode=disable"
+	"github.com/nandes007/simplebank/util"
 )
 
 // var testQueries *Queries
 var testStore Store
 
 func TestMain(m *testing.M) {
-	connPool, err := pgxpool.New(context.Background(), dbURL)
+	config, err := util.LoadConfig("../..")
+	if err != nil {
+		log.Fatal("cannot load config:", err)
+	}
+
+	connPool, err := pgxpool.New(context.Background(), config.DBUrl)
 	if err != nil {
 		log.Fatal("cannot connect to db:", err)
 	}
