b6fc8527c9fdfec00c55a4c606d3a7166432b64e
Nandes Simanjuntak <nandessimanjuntak1803@gmail.com>
2025-03-26 16:33:19 +0700

    add currency custom validator and add transfer api

---
+++ simplebank/util/currency.go
@@ -0,0 +1,18 @@
+package util
+
+// constants for all supported currency
+const (
+	USD = "USD"
+	EUR = "EUR"
+	IDR = "IDR"
+)
+
+// IsSupportedCurrency returns true if the currency is supported
+func IsSupportedCurrency(currency string) bool {
+	switch currency {
+	case USD, EUR, IDR:
+		return true
+	}
+
+	return false
+}
