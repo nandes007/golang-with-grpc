fb1e78f66268d8edaf0c08a0d4f421eef16f04b6
Nandes Simanjuntak <nandessimanjuntak1803@gmail.com>
2025-03-27 21:33:15 +0700

    jwt token maker

---
+++ simplebank/token/payload.go
@@ -0,0 +1,48 @@
+package token
+
+import (
+	"errors"
+	"time"
+
+	"github.com/golang-jwt/jwt/v5"
+	"github.com/google/uuid"
+)
+
+var (
+	ErrInvalidToken = errors.New("token is invalid")
+	ErrExpiredToken = errors.New("token has expired")
+)
+
+// Payload contains the payload data of the token.
+type Payload struct {
+	ID        uuid.UUID `json:"id"`
+	Username  string    `json:"username"`
+	IssueAt   time.Time `json:"issue_at"`
+	ExpiredAt time.Time `json:"expired_at"`
+	jwt.RegisteredClaims
+}
+
+// NewPayload creates a new payload for specific username and duration.
+func NewPayload(username string, duration time.Duration) (*Payload, error) {
+	tokenID, err := uuid.NewRandom()
+	if err != nil {
+		return nil, err
+	}
+
+	payload := &Payload{
+		ID:        tokenID,
+		Username:  username,
+		IssueAt:   time.Now(),
+		ExpiredAt: time.Now().Add(duration),
+	}
+
+	return payload, nil
+}
+
+func (payload *Payload) Validate() error {
+	if time.Now().After(payload.ExpiredAt) {
+		return ErrExpiredToken
+	}
+
+	return nil
+}
