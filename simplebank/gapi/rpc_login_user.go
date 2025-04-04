4097ca2ccecd9f3f5080b07abb7426a6d3d90088
Nandes Simanjuntak <nandessimanjuntak1803@gmail.com>
2025-04-04 23:43:57 +0700

    implement grpc create user and grpc login

---
+++ simplebank/gapi/rpc_login_user.go
@@ -0,0 +1,73 @@
+package gapi
+
+import (
+	"context"
+
+	"github.com/jackc/pgx/v5"
+	"github.com/jackc/pgx/v5/pgtype"
+	db "github.com/nandes007/simplebank/db/sqlc"
+	"github.com/nandes007/simplebank/pb"
+	"github.com/nandes007/simplebank/util"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
+	"google.golang.org/protobuf/types/known/timestamppb"
+)
+
+func (server *Server) LoginUser(ctx context.Context, req *pb.LoginUserRequest) (*pb.LoginUserResponse, error) {
+	user, err := server.store.GetUser(ctx, req.GetUsername())
+	if err != nil {
+		if err == pgx.ErrNoRows {
+			return nil, status.Errorf(codes.NotFound, "user not found: %s", err)
+		}
+
+		return nil, status.Errorf(codes.Internal, "failed to get user: %s", err)
+	}
+
+	err = util.CheckPassword(req.GetPassword(), user.HashedPassword)
+	if err != nil {
+		return nil, status.Errorf(codes.Internal, "password is missmatch: %s", err)
+	}
+
+	accessToken, accessPayload, err := server.tokenMaker.CreateToken(
+		user.Username,
+		server.config.AccessTokenDuration,
+	)
+	if err != nil {
+		return nil, status.Errorf(codes.Internal, "failed to create access token: %s", err)
+	}
+
+	refreshToken, refreshPayload, err := server.tokenMaker.CreateToken(
+		user.Username,
+		server.config.RefreshTokenDuration,
+	)
+	if err != nil {
+		return nil, status.Errorf(codes.Internal, "failed to create refresh token: %s", err)
+	}
+
+	session, err := server.store.CreateSession(ctx, db.CreateSessionParams{
+		ID:           refreshPayload.ID,
+		Username:     user.Username,
+		RefreshToken: refreshToken,
+		UserAgent:    "", // TODO:: fill this
+		ClientIp:     "", // TODO:: fill this
+		IsBlocked:    false,
+		ExpiresAt: pgtype.Timestamptz{
+			Time:  refreshPayload.ExpiredAt,
+			Valid: true,
+		},
+	})
+	if err != nil {
+		return nil, status.Errorf(codes.Internal, "failed to store user session: %s", err)
+	}
+
+	rsp := &pb.LoginUserResponse{
+		User:                  convertUser(user),
+		SessionId:             session.ID.String(),
+		AccessToken:           accessToken,
+		AccessTokenExpiresAt:  timestamppb.New(accessPayload.ExpiredAt),
+		RefreshToken:          refreshToken,
+		RefreshTokenExpiresAt: timestamppb.New(refreshPayload.ExpiredAt),
+	}
+
+	return rsp, nil
+}
