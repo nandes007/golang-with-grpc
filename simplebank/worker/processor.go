600cde49617b7453bb648c0b9e0c83c08ee8efc3
Nandes Simanjuntak <nandessimanjuntak1803@gmail.com>
2025-04-07 23:17:04 +0700

    add logger when process failed with custom zerologger

--- simplebank/worker/processor.go
+++ simplebank/worker/processor.go
@@ -5,6 +5,8 @@ import (
 
 	"github.com/hibiken/asynq"
 	db "github.com/nandes007/simplebank/db/sqlc"
+	"github.com/redis/go-redis/v9"
+	"github.com/rs/zerolog/log"
 )
 
 const (
@@ -23,6 +25,9 @@ type RedisTaskProcessor struct {
 }
 
 func NewRedisTaskProcessor(redisOpt asynq.RedisClientOpt, store db.Store) TaskProcessor {
+	logger := NewLogger()
+	redis.SetLogger(logger)
+
 	server := asynq.NewServer(
 		redisOpt,
 		asynq.Config{
@@ -30,6 +35,13 @@ func NewRedisTaskProcessor(redisOpt asynq.RedisClientOpt, store db.Store) TaskPr
 				QueueCritical: 10,
 				QueueDefault:  5,
 			},
+			ErrorHandler: asynq.ErrorHandlerFunc(func(ctx context.Context, task *asynq.Task, err error) {
+				log.Error().Err(err).
+					Str("type", task.Type()).
+					Bytes("payload", task.Payload()).
+					Msg("process task failed")
+			}),
+			Logger: NewLogger(),
 		},
 	)
 
