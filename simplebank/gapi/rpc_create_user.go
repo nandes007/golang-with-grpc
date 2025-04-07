10c0d40221cd435bc76e98166f016c76391fec61
Nandes Simanjuntak <nandessimanjuntak1803@gmail.com>
2025-04-07 22:12:03 +0700

    make queue label to be constant variable

--- simplebank/gapi/rpc_create_user.go
+++ simplebank/gapi/rpc_create_user.go
@@ -50,7 +50,7 @@ func (server *Server) CreateUser(ctx context.Context, req *pb.CreateUserRequest)
 	opts := []asynq.Option{
 		asynq.MaxRetry(10),
 		asynq.ProcessIn(10 * time.Second),
-		asynq.Queue("critical"),
+		asynq.Queue(worker.QueueCritical),
 	}
 	err = server.taskDistributor.DistrubuteTaskSendVerifyEmail(ctx, taskPayload, opts...)
 	if err != nil {
