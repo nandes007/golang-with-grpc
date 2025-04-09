a062c128beda8f324e3604961f737b55d8f421c2
Nandes Simanjuntak <nandessimanjuntak1803@gmail.com>
2025-04-09 23:56:31 +0700

    implement send email using smtp

---
+++ simplebank/mail/sender_test.go
@@ -0,0 +1,31 @@
+package mail
+
+import (
+	"testing"
+
+	"github.com/nandes007/simplebank/util"
+	"github.com/stretchr/testify/require"
+)
+
+func TestSendEmailWithGmail(t *testing.T) {
+	if testing.Short() {
+		t.Skip()
+	}
+
+	config, err := util.LoadConfig("..")
+	require.NoError(t, err)
+
+	sender := NewGmailSender(config.EmailSenderName, config.EmailSenderAddress, config.EmailSenderPassword)
+
+	subject := "A test email"
+	content := `
+	<h1>Hello world</h1>
+	<p>This is a test message from Nandestech</p>
+	`
+
+	to := []string{"fernandessimanjuntak787@gmail.com"}
+	attachFiles := []string{"../README.md"}
+
+	err = sender.SendEmail(subject, content, to, nil, nil, attachFiles)
+	require.NoError(t, err)
+}
